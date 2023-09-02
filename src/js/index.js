85b5e2eddb579f7b3172a9d5923f8966f60c57b8
Aistben <128066573+Aistben@users.noreply.github.com>
2023-09-02 13:33:14 +0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+
+import youtubeLightbox from "./modules/libs/youtubeLightbox.js";
+youtubeLightbox();
+
+
+import mask from './modules/libs/phoneMask/mask.js';
+mask();
+
+
+
+import mobileNav from "./modules/mobile-nav.js";
+mobileNav();
